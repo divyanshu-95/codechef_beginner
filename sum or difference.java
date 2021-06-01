40cc7ebb629f6312da162def9601ee739b5f911d
Divyanshu Varshney <51895957+divyanshu-95@users.noreply.github.com>
2021-06-01 17:13:02 +0530

    Create sum or difference.java

---
+++ sum or difference.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CodeChef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    try{
+		    Scanner s=new Scanner(System.in);
+		    int a=s.nextInt();
+		    int b=s.nextInt();
+		    if(a>b){
+		        System.out.println(a-b);
+		    }else{
+		        System.out.println(a+b);
+		    }
+		    
+		}catch(Exception e){
+	        
+	    }
+	}
+}
