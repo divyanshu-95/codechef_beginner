95ded6b8ae66450ffeb9cc850f2deb0b98429e78
Divyanshu Varshney <51895957+divyanshu-95@users.noreply.github.com>
2021-06-02 11:38:47 +0530

    Create is it a vowel or consonant.java

---
+++ is it a vowel or consonant.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class CodeChef {
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+             char c=s.next().charAt(0);
+            if(c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+                System.out.println("Vowel");
+            }else{
+                System.out.println("Consonant");
+            }
+    
+}
+}
